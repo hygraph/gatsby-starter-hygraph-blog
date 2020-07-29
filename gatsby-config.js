a8d7bf33e3649f015c55fb35eca5f0e2bdbec06d
Jonathan Steele <jonathan@steele.pro>
2020-07-29 12:09:37 +0100

    chore(deps): Add gatsby-image dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 
 module.exports = {
   plugins: [
+    'gatsby-plugin-sharp',
     'gatsby-plugin-postcss',
     {
       resolve: 'gatsby-source-graphcms',
       options: {
         endpoint: process.env.GRAPHCMS_ENDPOINT,
         token: process.env.GRAPHCMS_TOKEN,
+        downloadLocalImages: true,
       },
     },
+    'gatsby-transformer-sharp',
   ],
 }
