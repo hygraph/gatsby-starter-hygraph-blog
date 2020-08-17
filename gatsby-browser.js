6bbfb6f7194608eddf05763df4cf15768404a359
Jonathan Steele <jonathan@steele.pro>
2020-08-17 12:43:01 +0100

    feat: Add <Header />

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 
+import Header from './src/components/header'
+
 import './src/styles/index.css'
 
 const wrapPageElement = ({ element }) => (
   <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-5xl">
+    <Header />
     <main>{element}</main>
   </div>
 )
