e6e2b263200c18b41203123cf54372cc50adbd18
Jonathan Steele <jonathan@steele.pro>
2020-08-18 15:52:02 +0100

    fix: Import graphql tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
 function SEO({ title, seo }) {
