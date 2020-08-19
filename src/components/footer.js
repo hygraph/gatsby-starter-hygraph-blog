34b3d2d2d2b6502b176d5923f8f277f7c172eadb
Jonathan Steele <jonathan@steele.pro>
2020-08-19 11:00:26 +0100

    fix: Add title to <a>

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,18 +9,22 @@ const socialLinks = [
   {
     Component: GitHubSVG,
     href: 'https://github.com/graphcms/gatsby-graphcms-ecommerce-starter',
+    title: 'GitHub',
   },
   {
     Component: SlackSVG,
     href: 'http://slack.graphcms.com',
+    title: 'Slack',
   },
   {
     Component: TwitterSVG,
     href: 'https://twitter.com/graphcms',
+    title: 'Twitter',
   },
   {
     Component: LinkedInSVG,
     href: 'https://www.linkedin.com/company/graphcms',
+    title: 'LinkedIn',
   },
 ]
 
@@ -30,13 +34,14 @@ function Footer() {
       <div className="flex flex-col md:flex-row items-center md:justify-between py-6 max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-5xl space-y-6 md:space-y-0">
         <p className="text-gray-300">Powered by GraphCMS &amp; Gatsby</p>
         <ul className="inline-flex space-x-6">
-          {socialLinks.map(({ Component, href }, index) => (
+          {socialLinks.map(({ Component, href, title }, index) => (
             <li key={index}>
               <a
                 href={href}
                 target="_blank"
                 className="text-gray-300 hover:text-white text-sm"
                 rel="noopener noreferrer"
+                title={title}
               >
                 <Component className="h-6 w-6" />
               </a>
