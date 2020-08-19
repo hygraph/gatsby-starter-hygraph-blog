f68bb01025cfb624007c2fd0875220e89b37458e
Jonathan Steele <jonathan@steele.pro>
2020-08-19 10:58:12 +0100

    refactor: map over social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,60 +5,44 @@ import LinkedInSVG from '../svg/linkedin.svg'
 import SlackSVG from '../svg/slack.svg'
 import TwitterSVG from '../svg/twitter.svg'
 
+const socialLinks = [
+  {
+    Component: GitHubSVG,
+    href: 'https://github.com/graphcms/gatsby-graphcms-ecommerce-starter',
+  },
+  {
+    Component: SlackSVG,
+    href: 'http://slack.graphcms.com',
+  },
+  {
+    Component: TwitterSVG,
+    href: 'https://twitter.com/graphcms',
+  },
+  {
+    Component: LinkedInSVG,
+    href: 'https://www.linkedin.com/company/graphcms',
+  },
+]
+
 function Footer() {
   return (
     <footer className="bg-gray-800">
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-5xl">
-        <div className="flex flex-col md:flex-row items-center md:justify-between py-6">
-          <p className="text-gray-300 mb-3 md:mb-0">
-            Powered by GraphCMS &amp; Gatsby
-          </p>
-          <ul className="inline-flex space-x-4">
-            <li>
+      <div className="flex flex-col md:flex-row items-center md:justify-between py-6 max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-5xl space-y-6 md:space-y-0">
+        <p className="text-gray-300">Powered by GraphCMS &amp; Gatsby</p>
+        <ul className="inline-flex space-x-6">
+          {socialLinks.map(({ Component, href }, index) => (
+            <li key={index}>
               <a
-                href="https://github.com/graphcms/gatsby-graphcms-ecommerce-starter"
+                href={href}
                 target="_blank"
                 className="text-gray-300 hover:text-white text-sm"
                 rel="noopener noreferrer"
               >
-                <GitHubSVG className="h-5 w-5" />
+                <Component className="h-6 w-6" />
               </a>
             </li>
-
-            <li>
-              <a
-                href="http://slack.graphcms.com"
-                target="_blank"
-                className="text-gray-300 hover:text-white text-sm"
-                rel="noopener noreferrer"
-              >
-                <SlackSVG className="h-5 w-5" />
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="https://twitter.com/graphcms"
-                target="_blank"
-                className="text-gray-300 hover:text-white text-sm"
-                rel="noopener noreferrer"
-              >
-                <TwitterSVG className="h-5 w-5" />
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="https://www.linkedin.com/company/graphcms"
-                target="_blank"
-                className="text-gray-300 hover:text-white text-sm"
-                rel="noopener noreferrer"
-              >
-                <LinkedInSVG className="h-5 w-5" />
-              </a>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
     </footer>
   )
